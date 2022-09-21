1f7c5a8c38e7cf4dff200435674a9b5acb1258cf
MEEP00H <sophonjampasornklin@gmail.com>
2022-09-21 20:37:10 +0700

    fixed

--- components/Summary/Interview.jsx
+++ components/Summary/Interview.jsx
@@ -10,8 +10,8 @@ export const Interview = () => {
   return (
     <div className="relative min-h-screen pt-24 overflow-hidden ">
       <div className="absolute top-0  left-[50%] translate-x-[-50%]">
-        <Spline scene=" https://prod.spline.design/MkKIeHHUF5d1vao8/scene.splinecode" />
-        <Spline scene=" https://prod.spline.design/MkKIeHHUF5d1vao8/scene.splinecode" />
+        {/* <Spline scene=" https://prod.spline.design/MkKIeHHUF5d1vao8/scene.splinecode" />
+        <Spline scene=" https://prod.spline.design/MkKIeHHUF5d1vao8/scene.splinecode" /> */}
       </div>
       <div id="title" className="text-center ">
         <p className="font-bold b2">

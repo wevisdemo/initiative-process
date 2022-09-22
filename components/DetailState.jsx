e0a272bdf9de080e63b3071c49a8e470c236c571
MEEP00H <sophonjampasornklin@gmail.com>
2022-09-22 19:48:29 +0700

    change size detail state

--- components/DetailState.jsx
+++ components/DetailState.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const DetailState = ({ top, left, img, des }) => {
   return (
     <div
-      className="absolute text-left  -translate-x-[50%]  lg:-translate-x-[100%] w-[300px]  md:w-[320px] lg:w-[340px] bg-black z-40  rounded-[10px] p-3 lg:p-5"
+      className="absolute text-left  -translate-x-[50%]  lg:-translate-x-[100%] w-[310px]  md:w-[320px] lg:w-[340px] bg-black z-40  rounded-[10px] p-3 lg:p-5"
       style={{ top: top, background: "rgba(55, 55, 70, 0.75)", left: left }}
     >
       <div className="  relative w-[30px] h-[30px]">

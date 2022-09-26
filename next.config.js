62789748bb89cb8632067e3ac8d93e5c0bd271db
MEEEP00H <sophonjampasornklin@gmail.com>
2022-09-26 19:05:32 +0700

    fixed buid

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const withTM = require("next-transpile-modules")(["@wevisdemo/ui"]);
 const withVideos = require("next-videos");
 const nextConfig = withTM(
   withVideos({
-    basePath: "/initiative-process",
-    distDir: "initiative-process",
-    webpack(config, options) {
-      return config;
-    },
     reactStrictMode: false,
     swcMinify: true,
   })
